0805a89bae6867bee3399f90f266d1d58bdbc182
Immortalkitty <katarzyna.szczerba1112@gmail.com>
2023-12-31 16:25:50 +0100

    Documentation finish

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Detrasher"},{"p":"<Unnamed>","l":"Odpad"},{"p":"<Unnamed>","l":"OknoNicku"},{"p":"<Unnamed>","l":"PanelGry"},{"p":"<Unnamed>","l":"PanelMenu"},{"p":"<Unnamed>","l":"PanelSzans"},{"p":"<Unnamed>","l":"Odpad.TypySmieci"}];updateSearchResults();
